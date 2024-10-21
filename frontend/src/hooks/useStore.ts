9363fb43fd6f634da71081f0d8403298618db3e4
Juan_M <jmmarulanda2017@hotmail.com>
2024-10-21 01:02:51 -0500

    guardar cambios

--- frontend/src/hooks/useStore.ts
+++ frontend/src/hooks/useStore.ts
@@ -1,22 +1,25 @@
 // src/store/useStore.ts
 
-import { create } from 'zustand';
-import { MessageStore } from '../interfaces/MessageStore';
-import Message from '../interfaces/Message';
-import { io } from 'socket.io-client';
+import { create } from "zustand";
+import { MessageStore } from "../interfaces/MessageStore";
+import Message from "../interfaces/Message";
+import { io } from "socket.io-client";
 
-const socket = io('http://localhost:3000/');
+const socket = io(
+  "https://chat-converter-vj9v.vercel.app/?vercelToolbarCode=jJGtl1S0bv8s3NQ"
+);
 
 const useStore = create<MessageStore>((set) => ({
-    messages: [],
-    userName: '',
-    setUserName: (userName: string) => set({ userName }), // Puedes simplificar esto
-    addMessage: (message: Message) => set((state) => ({
-        messages: [...state.messages, message], 
+  messages: [],
+  userName: "",
+  setUserName: (userName: string) => set({ userName }), // Puedes simplificar esto
+  addMessage: (message: Message) =>
+    set((state) => ({
+      messages: [...state.messages, message],
     })),
-    chatStarted: false,
-    setChatStarted: (started: boolean) => set({ chatStarted: started }), // Corrige aquí
-    socket,
+  chatStarted: false,
+  setChatStarted: (started: boolean) => set({ chatStarted: started }), // Corrige aquí
+  socket,
 }));
 
 export default useStore;
